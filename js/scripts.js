9a3e875d2ad8a906e6acb4e999657ed40966a752
braulioportela79 <braulioportela@outlook.com>
2022-08-10 20:44:49 -0300

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+const body = document.body;
+const h1 = document.querySelector('h1');
+const item = document.getElementsByClassName('item');
+const itemText = document.querySelectorAll('.item h2, p');
+const toggleText = document.querySelector('.toggle p');
+const toggleInitialText = 'Dark Mode';
+// const btn = document.getElementById('btn');
+// }
+ const toggleTheme = () => {
+    body.classList.toggle('body-dark-mode');
+
+    h1.classList.toggle('h1-dark-mode');
+
+    for (let i = 0; i < item.length; i++) {
+        item[i].classList.toggle('item-dark-mode');
+    };
+
+    for (let i = 0; i < itemText.length; i++) {
+        itemText[i].classList.toggle('itemText-dark-mode');
+    };
+
+    if (toggleText.textContent.toLowerCase().includes(toggleInitialText.toLowerCase())) {
+        toggleText.textContent = 'Light Mode';
+    } else {
+        toggleText.textContent = toggleInitialText;
+    };
+
+    // btn.classList.toggle('button-dark-mode');
+
+    // if (btn.textContent.toLowerCase().includes(toggleInitialText.toLowerCase())) {
+    //     btn.textContent = 'Light Mode';
+    // } else {
+    //     btn.textContent = toggleInitialText;
+    // };
+};
